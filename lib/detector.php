02fa00b9155bd2139484d47661a5b5c605ed1c94
Ben Rush <orionrush@gmail.com>
2017-02-18 15:49:55 +0000

    Code formatting, and we broke apart some of the strings for the alerts into headings and descriptions.

--- lib/detector.php
+++ lib/detector.php
@@ -67,12 +67,17 @@ add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\\enqueue_dd_in_admin' );
  */
 
 function ajax_callback() {
-	$dupes_found_head_text = __( 'Whoa there! We found the following entries with a similar heading:', 'dupdetect' );
+	$dupes_found_head_notice = __( 'Whoa there!', 'orionrush_duplicate_detector' );
+	if ( has_filter( 'dd_dupes_response_head_notice' ) ) {
+		$dupes_found_head_notice = apply_filters( 'dd_dupes_response_head_text', $dupes_found_head_notice );
+	}
+
+	$dupes_found_head_text = __( 'We found the following titles:', 'orionrush_duplicate_detector' );
 	if ( has_filter( 'dd_dupes_response_head_text' ) ) {
 		$dupes_found_head_text = apply_filters( 'dd_dupes_response_head_text', $dupes_found_head_text );
 	}
 
-	$dupes_found_foot_text = sprintf( __( 'The title(s) listed above look very similar to this one. Consider making your title more specific, or perhaps move it to the trash. %s Also pay attention to your permalink for good SEO.', 'dupdetect' ), '<br/>' );
+	$dupes_found_foot_text = sprintf( __( 'Consider making this title a bit more unique. %s %sAlso consider your permalink below for good SEO.%s', 'dupdetect' ), '<br/>', '<em>', '</em>' );
 	if ( has_filter( 'dd_dupes_response_foot_text' ) ) {
 		$dupes_found_foot_text = apply_filters( 'dd_dupes_response_foot_text', $dupes_found_foot_text );
 	}
@@ -85,14 +90,36 @@ function ajax_callback() {
 	// Grab details from inbound POST array
 	$title      = $_POST['post_title'];
 	$post_id    = $_POST['post_id'];
-	$post_types = get_post_type( $post_id );
+	$post_types = [];
+	$post_type  = get_post_type( $post_id );
+
+
+	$options_array = get_option( 'orionrush_duplicate_detector' );
+
+	// Test to see if current post type should be isolated.
+	if ( in_array( $post_type, $options_array['post_types_isolate'] ) ) {
+		$post_types = $post_type;
+	} else {
+
+		$post_types_array = array_diff( $options_array['post_types'], $options_array['post_types_isolate'] );
+
+		// Expects a space separated list...
+		$post_types = (string) implode( ' ', $post_types_array );
+
+		\OrionRush\DuplicateDetector\Helpers\write_log( 'DD callback, post-types being searched:' );
+		\OrionRush\DuplicateDetector\Helpers\write_log( $post_types );
+	}
 
 	// Get any matches for post title
 	$sim_results = get_any_matches( $title, $post_id, 'publish', $post_types, '0' );
 
 	// if there are any matches
 	if ( $sim_results ) {
-		$notice = array( "head" => $dupes_found_head_text, "foot" => "$dupes_found_foot_text" );
+		$notice = array(
+			"head_notice" => $dupes_found_head_notice,
+			"head_text"   => $dupes_found_head_text,
+			"foot"        => "$dupes_found_foot_text"
+		);
 		foreach ( $sim_results as $sim_result ) {
 			$details['ID']   = $sim_result->ID;
 			$path            = 'post.php?post=' . $sim_result->ID . '&action=edit';
@@ -115,8 +142,8 @@ function ajax_callback() {
 	}
 
 	// Log these values if we're debugging
-	\OrionRush\DuplicateDetector\write_log( "DD Returned JSON as array:" );
-	\OrionRush\DuplicateDetector\write_log( $return_json );
+	\OrionRush\DuplicateDetector\Helpers\write_log( "DD Returned JSON as array:" );
+	\OrionRush\DuplicateDetector\Helpers\write_log( $return_json );
 
 	if ( ob_get_length() )
 		// Flush buffers to default point
